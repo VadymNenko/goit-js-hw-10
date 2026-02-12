1c7124dd51371883d346fb8d5f0363f90667fc1e
VadymNenko <vnenko14@gmail.comm>
2026-02-12 16:58:48 +0200

    HW-10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const delay = Number(formData.get('delay'));
+  const state = formData.get('state');
+
+  createPromise(delay, state)
+    .then(delayValue => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delayValue}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delayValue => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delayValue}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
